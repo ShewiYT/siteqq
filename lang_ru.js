36d9d3388a78f3fa64832995851a29fce2e19d8f
ShewiYT <71594547+ShewiYT@users.noreply.github.com>
2026-01-20 14:45:47 +0700

    Update lang_ru.js

--- lang_ru.js
+++ lang_ru.js
@@ -37,7 +37,7 @@ const translations_ru = {
         "filter_battle": "Баттл-рояли",
         "filter_other": "Другие",
         "top_cheats": "Топ читы",
-        "download_btn": "СКАЧАТЬ Loader",
+        "download_btn": "СКАЧАТЬ X-GEN.EXE",
         "downloading": "ЗАГРУЗКА...",
         
         // Описания читов для игр
@@ -114,4 +114,3 @@ const translations_ru = {
         "footer_copyright": "© 2026 X-GEN HACK. Все права защищены."
     }
 };
-
