28ac0f87f3c41e22b0daa651267fdce4394d908a
ShewiYT <71594547+ShewiYT@users.noreply.github.com>
2026-01-20 20:18:36 +0700

    Update lang_ru.js

--- lang_ru.js
+++ lang_ru.js
@@ -36,8 +36,8 @@ const translations_ru = {
         "filter_fps": "Шутеры",
         "filter_battle": "Баттл-рояли",
         "filter_other": "Другие",
-        "top_cheats": "Топ читы",
-        "download_btn": "СКАЧАТЬ X-GEN.EXE",
+        "top_cheats": "Undetected",
+        "download_btn": "СКАЧАТЬ ",
         "downloading": "ЗАГРУЗКА...",
         
         // Описания читов для игр
@@ -114,3 +114,4 @@ const translations_ru = {
         "footer_copyright": "© 2026 X-GEN HACK. Все права защищены."
     }
 };
+
